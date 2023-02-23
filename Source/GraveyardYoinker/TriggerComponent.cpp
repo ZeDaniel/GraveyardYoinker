0ffae53ec14d18b488351f3916fcd07e096c8edb
Daniel Mains <zedaniel101@gmail.com>
2023-02-22 18:03:02 -0600

    Add Overlap tick to Trigger Component Add Acceptable Actor Tag to Trigger Component Use Tage for Overlap tick

--- Source/GraveyardYoinker/TriggerComponent.cpp
+++ Source/GraveyardYoinker/TriggerComponent.cpp
@@ -3,10 +3,33 @@
 
 #include "TriggerComponent.h"
 
+// Sets default values for this component's properties
+UTriggerComponent::UTriggerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
 // Called when the game starts
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Display, TEXT("Trigger Component reporting for duty"));
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	TArray<AActor*> Actors;
+	GetOverlappingActors(Actors);
+
+	for (AActor* Actor : Actors)
+	{
+		if(Actor->ActorHasTag(AcceptableActorTag))
+			UE_LOG(LogTemp, Display, TEXT("Unlocking"));
+	}
 }
--- Source/GraveyardYoinker/TriggerComponent.h
+++ Source/GraveyardYoinker/TriggerComponent.h
@@ -14,8 +14,18 @@ class GRAVEYARDYOINKER_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
 
+public:
+	// Sets default values for this component's properties
+	UTriggerComponent();
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	FName AcceptableActorTag;
 };
